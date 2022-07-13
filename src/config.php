2d7d7f759ef6168d4ad8dd15abc3aa2c80be2b2c
Kuroyuki-Kaze <teng.thaisothyrak@gmail.com>
2022-07-13 20:05:31 +0700

    more internationalization

--- src/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $server="localhost";
-    $username="root";
-    $password="loco0117";
-    $database="webtest";
-
-?>
--- src/views/main_page.php
+++ src/views/main_page.php
@@ -85,7 +85,34 @@
     <link rel="stylesheet" href="../resources/css/main.css">
     <title>Product Page</title>
 
-    <script class="init"> $(document).ready(() => { $('#example').DataTable(); }); </script>
+    <script class="init">
+        $(document).ready(() => { $('#example').DataTable(
+            {
+                language: {
+                    "emptyTable": "<?php echo $GLOBALS['tr']->get('dt.empty_table'); ?>",
+                    "info": "<?php echo $GLOBALS['tr']->get('dt.info'); ?>",
+                    "infoEmpty": "<?php echo $GLOBALS['tr']->get('dt.info_empty'); ?>",
+                    "infoFiltered": "<?php echo $GLOBALS['tr']->get('dt.info_filtered'); ?>",
+                    "thousands": "<?php echo $GLOBALS['tr']->get('dt.thousands'); ?>",
+                    "lengthMenu": "<?php echo $GLOBALS['tr']->get('dt.length_menu'); ?>",
+                    "loadingRecords": "<?php echo $GLOBALS['tr']->get('dt.loading_records'); ?>",
+                    "processing": "<?php echo $GLOBALS['tr']->get('dt.processing'); ?>",
+                    "search": "<?php echo $GLOBALS['tr']->get('dt.search'); ?>",
+                    "zeroRecords": "<?php echo $GLOBALS['tr']->get('dt.zero_records'); ?>",
+                    "paginate": {
+                        "first": "<?php echo $GLOBALS['tr']->get('dt.paginate.first'); ?>",
+                        "last": "<?php echo $GLOBALS['tr']->get('dt.paginate.last'); ?>",
+                        "next": "<?php echo $GLOBALS['tr']->get('dt.paginate.next'); ?>",
+                        "previous": "<?php echo $GLOBALS['tr']->get('dt.paginate.previous'); ?>"
+                    },
+                    "aria": {
+                        "sortAscending": "<?php echo $GLOBALS['tr']->get('dt.aria.sort_ascending'); ?>",
+                        "sortDescending": "<?php echo $GLOBALS['tr']->get('dt.aria.sort_descending'); ?>"
+                    }
+                }
+            }
+        ); });
+    </script>
 </head><body>
     <div class="c-navbar">
 
@@ -104,10 +131,25 @@
                     <div class="c-ralign">
                         <!-- navbar items (Add product and a dropdown settings) -->
                         <div class="c-nav-item">
-                            <button onclick="toggleAddProduct(<?php echo $GLOBALS['user_id'] ?>)" class="btn btn-secondary">
+                            <button onclick="toggleAddProduct(<?php echo $GLOBALS['user_id'] ?>)" class="btn btn-primary">
                                 <i class="bi bi-node-plus"></i>
-                                <?php echo $GLOBALS['tr']->get('add_product') ?>
+                                <?php echo $GLOBALS['tr']->get('add_products') ?>
                             </button>
+                        </div>
+                        <div class="c-nav-item">
+                            <div class="change-language">
+                                <div class="dropdown">
+                                    <button class="btn btn-secondary dropdown-toggle" type="button" id="changeLangMenuButton" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        <?php echo $GLOBALS['tr']->get("change_language") ?>
+                                    </button>
+                                    <div class="dropdown-menu" aria-labelledby="changeLangMenuButton">
+                                        <a href="main_page.php?lang=en" class="dropdown-item">English</a>
+                                        <a href="main_page.php?lang=eo" class="dropdown-item">Esperanto</a>
+                                        <a href="main_page.php?lang=zh-ht" class="dropdown-item">繁體中文</a>
+                                        <a href="main_page.php?lang=jp" class="dropdown-item">日本語</a>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                         <div class="c-nav-item">
                             <div class="settings">
@@ -132,11 +174,11 @@
             <table id="example" class="table table-striped" style="width:100%">
                 <thead>
                     <tr>
-                        <th>Product ID</th>
-                        <th>Name</th>
-                        <th>Amount</th>
-                        <th>Price</th>
-                        <th>Edit</th>
+                        <th><?php echo $GLOBALS['tr']->get('th.product_id') ?></th>
+                        <th><?php echo $GLOBALS['tr']->get('th.product_name') ?></th>
+                        <th><?php echo $GLOBALS['tr']->get('th.product_quantity') ?></th>
+                        <th><?php echo $GLOBALS['tr']->get('th.product_price') ?></th>
+                        <th><?php echo $GLOBALS['tr']->get('th.actions') ?></th>
                     </tr>
                 </thead>
                 <tbody>
@@ -151,7 +193,7 @@
                             echo "<td>".$row['name']."</td>";
                             echo "<td>".$row['amount']."</td>";
                             echo "<td>$".$price."</td>";
-                            echo "<td><button onclick='toggleEdit($row_encode)' id='btn-edit' class='btn btn-primary'><i class='bi bi-pencil-square'></i>&nbsp;&nbsp;Edit</button>&nbsp;&nbsp;<button onclick='toggleDelete($row_encode)' id='btn-delete' class='btn btn-danger'><i class='bi bi-trash'></i>&nbsp;&nbsp;Delete</td>";
+                            echo "<td><button onclick='toggleEdit($row_encode)' id='btn-edit' class='btn btn-primary'><i class='bi bi-pencil-square'></i>&nbsp;&nbsp;".$GLOBALS['tr']->get('th.actions.edit')."</button>&nbsp;&nbsp;<button onclick='toggleDelete($row_encode)' id='btn-delete' class='btn btn-danger'><i class='bi bi-trash'></i>&nbsp;&nbsp;".$GLOBALS['tr']->get('th.actions.delete')."</td>";
                             $i++;
                         }
                     ?>
@@ -169,29 +211,30 @@
         var product_price = row.price;
 
         const { value: formValues } = await Swal.fire({
-            title: 'Edit the row',
+            title: <?php echo "'".$GLOBALS['tr']->get('swal.edit.title')."'" ?>,
             showDenyButton: true,
             showCancelButton: true,
-            confirmButtonText: 'Confirm',
-            denyButtonText: `Discard`,
+            confirmButtonText: <?php echo "'".$GLOBALS['tr']->get('swal.edit.confirm')."'" ?>,
+            denyButtonText: <?php echo "'".$GLOBALS['tr']->get('swal.edit.discard')."'" ?>,
+            cancelButtonText: <?php echo "'".$GLOBALS['tr']->get('swal.edit.cancel')."'" ?>,
             width: '700px',
             html:
                 '<table class="w-100">'+
                 '<tr>'+
-                '<td class="text-left">'+'<label for="id">Product id :</label>'+ '</td>'+
+                '<td class="text-left">'+'<label for="id"><?php echo $GLOBALS['tr']->get('th.product_id') ?> :</label>'+ '</td>'+
                 '<td>'+`<input id="pd_id" class="swal2-input" name="id" value=${product_id} disabled>` +'</td>'+
                 '</tr>'+
                 '<tr>'+
-                '<td class="text-right">'+ '<label for="product_name">Product name :</label>'+'</td>'+
+                '<td class="text-right">'+ '<label for="product_name"><?php echo $GLOBALS['tr']->get('th.product_name') ?> :</label>'+'</td>'+
                 '<td>'+`<input id="pd_name" class="swal2-input" name="product_name" value=${product_name}>`+'</td>'+
                 '</tr>'+
                 '</tr>'+
                 '<tr>'+
-                '<td class="text-right">'+ '<label for="product_qty">Product qty :</label>'+'</td>'+
+                '<td class="text-right">'+ '<label for="product_qty"><?php echo $GLOBALS['tr']->get('th.product_quantity') ?> :</label>'+'</td>'+
                 '<td>'+`<input id="pd_qty" class="swal2-input" name="product_qty" value=${product_qty}>` +'</td>'+
                 '</tr>'+
                 '<tr>'+
-                '<td class="text-right">'+'<label for="product_price">Product price :</label>' +'</td>'+
+                '<td class="text-right">'+'<label for="product_price"><?php echo $GLOBALS['tr']->get('th.product_price') ?> :</label>' +'</td>'+
                 '<td>'+ `<input id="pd_price" class="swal2-input" name="product_price" value=${product_price/100}>`+'</td>'+
                 '</tr>'+
                 '</table>',
@@ -213,17 +256,17 @@
 
                 if(!isValidName(formValues.pd_name) || !isValidPrice(formValues.pd_price) || !isValidQty(qty) || !onlyNumberQty){
                     Swal.fire(
-                        'Deny Change',
-                        'Some input changed may cause error! Please try again.',
+                        '<?php echo $GLOBALS["tr"]->get("swal.edit.confirm.error.title") ?>',
+                        '<?php echo $GLOBALS["tr"]->get("swal.edit.confirm.error.text") ?>',
                         'error'
                     );
                 }else{
                     Swal.fire({
                         icon: 'question',
-                        title: 'Are you sure you want to Save the change?',
+                        title: '<?php echo $GLOBALS['tr']->get("swal.edit.confirm.title") ?>',
                         showDenyButton: true,
-                        confirmButtonText: 'Save',
-                        denyButtonText: `Don't save`,
+                        confirmButtonText: '<?php echo $GLOBALS['tr']->get("swal.edit.confirm.save") ?>',
+                        denyButtonText: "<?php echo $GLOBALS['tr']->get("swal.edit.confirm.cancel") ?>",
                         }).then((result) => {
                         /* Read more about isConfirmed, isDenied below */
                         if (result.isConfirmed) {
@@ -233,13 +276,13 @@
                                 pd_qty : qty,
                                 pd_price: price
                             });
-                            Swal.fire('Saved!', '', 'success').then((result) => {
+                            Swal.fire('<?php echo $GLOBALS['tr']->get("swal.edit.confirm.save.title") ?>', '', 'success').then((result) => {
                                     window.location.href = "./main_page.php";
                                 }
                             );
                             
                         } else if (result.isDenied) {
-                            Swal.fire('Changes are not saved', '', 'info')
+                            Swal.fire('<?php echo $GLOBALS['tr']->get("swal.edit.confirm.cancel.title") ?>', '', 'info')
                         }
                     });
                 }
@@ -247,7 +290,7 @@
                 
                 // Swal.fire('Saved!', '', 'success')
             } else if (result.isDenied) {
-                Swal.fire('Changes are not saved', '', 'info')
+                Swal.fire('<?php echo $GLOBALS['tr']->get("swal.edit.confirm.cancel.title") ?>', '', 'info')
             }
     }
 
@@ -299,13 +342,14 @@
     function toggleDelete(row){
         var product_id = row.id;
         Swal.fire({
-        title: 'Are you sure?',
-        text: "You won't be able to revert this!",
+        title: '<?php echo $GLOBALS['tr']->get('swal.delete.title') ?>',
+        text: "<?php echo $GLOBALS['tr']->get('swal.delete.text') ?>",
         icon: 'warning',
         showCancelButton: true,
         confirmButtonColor: '#3085d6',
         cancelButtonColor: '#d33',
-        confirmButtonText: 'Yes, delete it!'
+        confirmButtonText: '<?php echo $GLOBALS['tr']->get('swal.delete.confirm') ?>',
+        cancelButtonText: '<?php echo $GLOBALS['tr']->get('swal.delete.cancel') ?>',
         }).then((result) => {
         if (result.isConfirmed) {
             deleteDataFromDataBase({
@@ -313,8 +357,8 @@
             });
 
             Swal.fire(
-            'Deleted!',
-            'Your file has been deleted.',
+            '<?php echo $GLOBALS['tr']->get('swal.delete.confirm.title') ?>',
+            '<?php echo $GLOBALS['tr']->get('swal.delete.confirm.text') ?>',
             'success'
             ).then((result) => {
                     window.location.href = "./main_page.php";
@@ -345,31 +389,31 @@
     }
     async function toggleAddProduct(user_id) {
         const { value: formValues } = await Swal.fire({
-            title: 'Add Product',
+            title: '<?php echo $GLOBALS['tr']->get('swal.add.title') ?>',
             html: `
             <table class="w-100">
             <tr>
-            <td class="text-left"><label for="user_id">User ID :</label></td>
+            <td class="text-left"><label for="user_id"><?php echo $GLOBALS['tr']->get('th.user_id') ?> :</label></td>
             <td><input id="add_user_id" class="swal2-input" name="user_id" value=${user_id} readonly></td>
             </tr>
             <tr>
-            <td class="text-left"><label for="product_name">Product name :</label></td>
+            <td class="text-left"><label for="product_name"><?php echo $GLOBALS['tr']->get('th.product_name') ?> :</label></td>
             <td><input id="add_product_name" class="swal2-input" name="product_name" value=""></td>
             </tr>
             <tr>
-            <td class="text-left"><label for="product_qty">Product qty :</label></td>
+            <td class="text-left"><label for="product_qty"><?php echo $GLOBALS['tr']->get('th.product_quantity') ?> :</label></td>
             <td><input id="add_product_qty" class="swal2-input" name="product_qty" value=""></td>
             </tr>
             <tr>
-            <td class="text-left"><label for="product_price">Product price :</label></td>
+            <td class="text-left"><label for="product_price"><?php echo $GLOBALS['tr']->get('th.product_price') ?> :</label></td>
             <td><input id="add_product_price" class="swal2-input" name="product_price" value=""></td>
             </tr>
             </table>`,
             showCancelButton: true,
             confirmButtonColor: '#3085d6',
             cancelButtonColor: '#d33',
-            confirmButtonText: 'Add',
-            cancelButtonText: 'Cancel',
+            confirmButtonText: '<?php echo $GLOBALS['tr']->get('swal.add.confirm') ?>',
+            cancelButtonText: '<?php echo $GLOBALS['tr']->get('swal.add.cancel') ?>',
             showLoaderOnConfirm: true,
             width: '700px',
             focusConfirm: false,
@@ -393,11 +437,11 @@
             let priceValidity = isValidPrice(price);
             
             if (!nameValidity || !qtyValidity || !priceValidity || !qtyContainsOnlyNumber || !priceContainsOnlyNumber) {
-                Swal.fire('Denying to add product!', "Please check your input", 'error');
+                Swal.fire('<?php echo $GLOBALS['tr']->get('swal.add.confirm.error.title') ?>', "<?php echo $GLOBALS['tr']->get('swal.add.confirm.error.text') ?>", 'error');
                 return;
             } else {
                 Swal.fire({
-                    title: "Are you sure to add this data?",
+                    title: '<?php echo $GLOBALS['tr']->get('swal.add.confirm.title') ?>',
                     html: `
                     <p>User ID : ${formValues.user_id}</p>
                     <p>Product name : ${formValues.product_name}</p>
@@ -407,7 +451,8 @@
                     showCancelButton: true,
                     confirmButtonColor: '#3085d6',
                     cancelButtonColor: '#d33',
-                    confirmButtonText: 'Yes, add it!'
+                    confirmButtonText: '<?php echo $GLOBALS['tr']->get('swal.add.confirm.save') ?>',
+                    cancelButtonText: '<?php echo $GLOBALS['tr']->get('swal.add.confirm.cancel') ?>',
                 }).then((result) => {
                     if (result.isConfirmed) {
                         let realPrice = Math.round(price * 100);
@@ -421,25 +466,25 @@
 
                         if (resp == 200) {
                             Swal.fire(
-                                'Added!',
-                                'Your file has been added.',
+                                '<?php echo $GLOBALS['tr']->get('swal.add.confirm.save.success.title') ?>',
+                                '<?php echo $GLOBALS['tr']->get('swal.add.confirm.save.success.text') ?>',
                                 'success'
                             ).then((result) => {
                                     window.location.href = "./main_page.php";
                                 }
                             );
                         } else if (resp == 403) {
-                            Swal.fire('Forbidden!', "You don't have permission to add this data", 'error');
+                            Swal.fire('<?php echo $GLOBALS['tr']->get('swal.add.confirm.save.forbidden.title') ?>', "<?php echo $GLOBALS['tr']->get('swal.add.confirm.save.forbidden.text') ?>", 'error');
                         } else {
-                            Swal.fire('Error!', "Something went wrong", 'error');
+                            Swal.fire('<?php echo $GLOBALS['tr']->get('swal.add.confirm.save.error.title') ?>', "<?php echo $GLOBALS['tr']->get('swal.add.confirm.save.error.text') ?>", 'error');
                         }
                     } else {
-                        Swal.fire('Cancelled', 'Your changes wasn\'t saved', 'info');
+                        Swal.fire('<?php echo $GLOBALS['tr']->get('swal.add.confirm.cancel.title') ?>', "<?php echo $GLOBALS['tr']->get('swal.add.confirm.cancel.text') ?>", 'info');
                     }
                 });
             }
         } else {
-            Swal.fire('Cancelled', 'Your changes wasn\'t saved', 'info');
+            Swal.fire('<?php echo $GLOBALS['tr']->get('swal.add.confirm.cancel.title') ?>', "<?php echo $GLOBALS['tr']->get('swal.add.confirm.cancel.text') ?>", 'info');
         }
     }
 
